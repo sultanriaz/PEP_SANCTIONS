37e02ea64d6bbee38d5dd27984ee3d3089087c4a
sultanriaz <superior7891@gmail.com>
2026-06-26 00:23:39 +0500

    PEP screening/Normalizer

--- SRC/Data_Normalizer.py
+++ SRC/Data_Normalizer.py
@@ -4,4 +4,25 @@ def remove_accents(text):
     if not text:
         return ""
     text = unicodedata.normalize('NFKD', text)
-    
+    result=" "
+    for char in text:
+        if not unicodedata.combining(char):
+            result+=char
+    return result
+def normalize_name(name):
+    if not name:
+        return " "
+    name=remove_accents(name)
+    name=name.lower()
+    name=re.sub(r'[^a-z0-9\s]', " ", name)
+    return name
+def split_aliases(aliase_text):
+    if not aliase_text:
+        return []
+    aliases=aliase_text.split("|")
+    normalized_aliases=[]
+    for alias in aliases:
+        normalized_alias=normalize_name(alias)
+        if normalized_alias:
+            normalized_aliases.append(normalized_alias)
+    return normalized_aliases
